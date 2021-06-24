0e4afdf0b2d41fbe1522c7377b35fee020aa0536
nefisik <43620307+nefisik@users.noreply.github.com>
2021-06-24 15:30:29 +0300

    changed answers when n == 0, n == 1

--- sum.cpp
+++ sum.cpp
@@ -2,8 +2,8 @@
 
 // sum(n) = 1 + (1+2)*2^1 + (1+2+3)*2^2 + ... + (1+2+3+...+n)*2^(n-1)
 long long int sum(int n) {
-	if(n == 0) return 1;
-	if(n == 1) return 2;
+	if(n == 0) return 0;
+	if(n == 1) return 1;
 	int sum = 1;
 	long long int result = 1;
 	for(int i = 2; i <= n; ++i) {
